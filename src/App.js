5d2634d12aaa6a279de9fc883df32afa6e4066a3
Hitesh Khatri <hiteshkumarkhatri7410@gmail.com>
2024-07-02 16:16:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import useCustomMemo from './hooks/use-custom-memo';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+  const [counter2, setCounter2] = useState(100);
+
+  const squaredValue = () => {
+    console.log("expensive Calculation...");
+    return counter * counter;
+  };
+
+  const memoisedSquaredValue = useCustomMemo(squaredValue,[counter]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>Counter: {counter}</h2>
+      <h2>Squared Counter: {memoisedSquaredValue}</h2>
+      <button onClick={() => setCounter(counter + 1)}>Increment</button>
+      <h2>Counter2: {counter2}</h2>
+      <button onClick={() => setCounter2(counter2 - 1)}>Decrement</button>
     </div>
   );
 }
